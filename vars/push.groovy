77e5b795fe5e5b9c43772682594b5b86ac53d161
shubham9817 <shubham78905498@gmail.com>
2026-04-07 13:45:33 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,1 +1,13 @@
 
+
+def call(String credentials){
+    echo 'pushing the image to dockerhub'
+    withCredentials([usernamePassword(
+      credentialID: ${credentials}, 
+      passwordVariable: 'dockerHubPass', 
+      usernameVariable: 'dockerHubUser')])
+                    
+    sh 'docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}' 
+    sh 'docker image tag django_app:latest ${env.dockerHubUser}/django_app:latest'
+    sh 'docker push ${env.dockerHubUser}/django_app:latest'
+}
